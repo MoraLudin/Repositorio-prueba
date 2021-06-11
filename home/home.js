47b504b10a3d58cd951baa5568f00619cbe024f3
Agustina <abreitfeld@udesa.edu.ar>
2021-06-11 14:48:04 -0300

    agregar imagen api

--- home/home.js
+++ home/home.js
@@ -1,3 +1,20 @@
 window.addEventListener('load', function(){
-    alert("hola mundo")
+    const proxy = 'https://cors-anywhere.herokuapp.com/';
+    
+/* tracks */
+/* peaches track */
+    let peaches = `${proxy}https://api.deezer.com/track/1280165222`;
+
+ fetch(peaches)
+    .then(function(response){
+        return response.json();
+    })
+    .then(function(datos){
+        console.log(datos);
+        let peachestrack = document.querySelector(".trackpeaches");
+        peachestrack.innerHTML += `<img class="peaches" src= "${datos.album.cover_medium}">`;
+    })
+    .catch(function(error){ 
+        console.log('Tu error es:' +error)
+    })
 })
