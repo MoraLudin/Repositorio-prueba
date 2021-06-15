a4a6f910a099a5b43093d783ceba014e37609307
Agustina <abreitfeld@udesa.edu.ar>
2021-06-15 00:13:57 -0300

    info listas

--- home/home.js
+++ home/home.js
@@ -4,28 +4,48 @@ window.addEventListener('load', function(){
 /* tracks */
 /* peaches track */
     let peaches = `${proxy}https://api.deezer.com/track/1280165222`;
-
+   
  fetch(peaches)
     .then(function(response){
         return response.json();
     })
     .then(function(datos){
         console.log(datos);
+
     /* imagen y info track peaches desde api */
         let peachestrackimg = document.querySelector(".trackpeaches");
         peachestrackimg.innerHTML += `
         <img class="peaches" src= "${datos.album.cover_medium}"> 
-        <h3>${datos.title}</h3> 
+        <h3 class="datos">${datos.title}, ${datos.artist.name}</h3> 
         `;
     
-    /* no fucniona la info */
+    })
+    .catch(function(error){ 
+        console.log('Tu error es:' +error)
+    })
 
-    /*     let peachestrackinfo = document.querySelector(".infopeaches");
-        peachestrackinfo.innerHTML += `<h3>${datos.title}</h3>`; */
 
+/* alaire track */
+let alaire = `${proxy}https://api.deezer.com/track/1133364592`;
 
+fetch(alaire)
+    .then(function(response){
+        return response.json();
+    })
+    .then(function(datos){
+        console.log(datos);
+
+    /* imagen y info track alaire */
+        let alairetrackimg = document.querySelector(".trackalaire");
+        alairetrackimg.innerHTML += `
+        <img class="alaire" src= "${datos.album.cover_medium}"> 
+        <h3 class="datos">${datos.title}, ${datos.artist.name}</h3>
+        `;
+    
     })
     .catch(function(error){ 
         console.log('Tu error es:' +error)
     })
 })
+
+
