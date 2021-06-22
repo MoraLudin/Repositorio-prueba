57d148c5f404f97341916a6be4c8215a8e4072c2
Maria Varas <mariavaras@MacBook-Pro-de-Maria.local>
2021-06-22 11:20:50 -0300

    search ok

---
+++ JAVASCRIPT/search-results.js
@@ -0,0 +1,31 @@
+let querystring = location.search;
+let datos = new URLSearchParams(querystring);
+let resultados = datos.get("search");
+
+let titulo = document.querySelector(".semuestran")
+titulo.innerHTML='Se muestran resultados para: '+resultados+''
+
+fetch("https://cors-anywhere.herokuapp.com/https://api.deezer.com/search/track?q="+resultados) 
+.then(function(response){
+    return response.json();
+})
+.then (function(data){
+   
+
+let busqueda = data.data
+console.log(busqueda);
+if ( busqueda.length == 0) {
+    titulo.innerHTML='No encontramos nada para: '+resultados+''
+}
+
+let lista = document.querySelector(".lista")
+
+for (let index = 0; index < busqueda.length; index++) {
+    lista.innerHTML += '<a href="detail-track.html?songId='+busqueda[index].id+'"><li> '+busqueda[index].title+', '+busqueda[index].artist.name+' </li></a>'
+   
+}
+
+
+
+
+})
