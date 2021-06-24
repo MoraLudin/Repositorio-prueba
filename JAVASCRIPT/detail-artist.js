8f8d643b9aac0a3bb8454e712d66d78d02b58be3
Agustina <abreitfeld@udesa.edu.ar>
2021-06-24 00:12:34 -0300

    arreglos css y detalles

--- JAVASCRIPT/detail-artist.js
+++ JAVASCRIPT/detail-artist.js
@@ -34,30 +34,14 @@ window.addEventListener('load', function(){
         .then(function(datos){
             console.log(datos)
 
-            let topSong1 = document.querySelector('.top5')
-            topSong1.innerHTML += `
-            <div class="divtop5"><a href="../HTML/detail-track.html?songId=${datos.data[0].id}"><h3>${datos.data[0].title}</h3></a></div>
-            `;
+            let albumTop = datos.data
+            for (let index = 0; index < 5; index++) {
+                let topSong1 = document.querySelector('.top5')
+                topSong1.innerHTML += `
+                <div class="divtop5"><a href="../HTML/detail-track.html?songId=${albumTop[index].id}"><h3>${albumTop[index].title}</h3></a></div>
+                `;  
+            }
 
-            let topSong2 = document.querySelector('.top5b')
-            topSong2.innerHTML += `
-            <div class="divtop5b"><a href="../HTML/detail-track.html?songId=${datos.data[1].id}"><h3>${datos.data[1].title}</h3></a></div> 
-            `;
-
-            let topSong3 = document.querySelector('.top5c')
-            topSong3.innerHTML += `
-            <div class="divtop5c"><a href="../HTML/detail-track.html?songId=${datos.data[2].id}"><h3>${datos.data[2].title}</h3></a></div> 
-            `;
-
-            let topSong4 = document.querySelector('.top5d')
-            topSong4.innerHTML += `
-            <div class="divtop5d"><a href="../HTML/detail-track.html?songId=${datos.data[3].id}"><h3>${datos.data[3].title}</h3></a></div> 
-            `;
-
-            let topSong5 = document.querySelector('.top5e')
-            topSong5.innerHTML += `
-            <div class="divtop5e"><a href="../HTML/detail-track.html?songId=${datos.data[4].id}"><h3>${datos.data[4].title}</h3></a></div> 
-            `;
         })
         .catch(function(error){
             console.log('Tu error es:' +error)
